e8c43fb782288aa69cfc66e6630b317efdf9f58a
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-10 16:12:31 +0100

    imported-store/AppCard/done-v-for

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,27 @@
 <script>
+import { store } from '../store'
+import AppCardFilm from './AppCardFilm.vue';
+
+export default {
+    components: {
+        AppCardFilm
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <main>
+        <section>
+            <div v-for="(items, i) in store.films" :key="i">
+                <AppCardFilm :detail="items" />
+            </div>
+        </section>
+    </main>
+</template>
 
 <style lang="scss" scoped></style>
