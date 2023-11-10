9be4e4deea32f74259a95ecfa47e9ba484c43add
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-10 17:39:10 +0100

    imported-appcard

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,12 @@
 <script>
 import { store } from '../store'
 import AppCardFilm from './AppCardFilm.vue';
+import AppCardSerie from './AppCardSerie.vue';
 
 export default {
     components: {
-        AppCardFilm
+        AppCardFilm,
+        AppCardSerie
     },
     data() {
         return {
@@ -15,13 +17,33 @@ export default {
 </script>
 
 <template>
-    <main class="row container">
-        <section>
+    <main class=" container card">
+        <section class="card_film">
             <div v-for="(items, i) in store.films" :key="i">
                 <AppCardFilm :detail="items" />
+            </div>
+        </section>
+        <section>
+            <div class="card_series" v-for="(show, i) in store.series" :key="i">
+                <AppCardSerie :showsInfo="show" />
             </div>
         </section>
     </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+    display: flex;
+}
+
+.card_film {
+    flex-basis: calc(100 / 12) * 6;
+    color: red;
+    border: 1px solid black;
+}
+
+.card_series {
+    flex-basis: calc(100 / 12) * 6;
+    border: 1px solid black;
+}
+</style>
