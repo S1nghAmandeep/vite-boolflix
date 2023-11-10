5129dbc32e10bfb5f10117418579c0a79d4ac4f2
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-10 16:11:39 +0100

    add-input/button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,24 @@
 <script>
+import { store } from '../store'
 
+export default {
+
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <header>
+        <input type="text" @keyup.enter="$emit('search')" v-model="store.query">
+        <button @click="$emit('search')">Serach</button>
+    </header>
+</template>
 
 <style lang="scss" scoped></style>
