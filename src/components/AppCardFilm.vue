7367d5dc616370197fa8f08f8b38242319a155ff
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-10 16:12:55 +0100

    add-props

---
+++ src/components/AppCardFilm.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+    props: {
+        detail: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+        console.log(this.detail);
+    }
+}
+</script>
+
+<template>
+    <h4>{{ detail.title }}</h4>
+    <h4>{{ detail.original_title }}</h4>
+    <p>{{ detail.original_language }}</p>
+    <p>{{ detail.vote_average }}</p>
+    <h1>ciao</h1>
+</template>
+
+<style lang="scss" scoped></style>
