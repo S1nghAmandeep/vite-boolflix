aba2dbe18f7e91091f0dac255659cb819952b342
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-10 17:38:58 +0100

    add-props/html-tags

--- src/components/AppCardSerie.vue
+++ src/components/AppCardSerie.vue
@@ -1,6 +1,12 @@
 <script>
 
 export default {
+    props: {
+        showsInfo: {
+            type: Object,
+            required: true
+        }
+    },
 
     data() {
         return {
@@ -12,6 +18,16 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <h4>{{ showsInfo.name }}</h4>
+    <h4>{{ showsInfo.original_name }}</h4>
+    <div>
+        <img v-if="showsInfo.original_language === 'uk'" class="flags" src="uk.png" alt="">
+        <img v-else-if="showsInfo.original_language === 'en'" class="flags" src="America.png" alt="">
+        <img v-else-if="showsInfo.original_language === 'it'" class="flags" src="italia.png" alt="">
+        <p v-else>{{ showsInfo.original_language }}</p>
+    </div>
+    <p>{{ showsInfo.vote_average }}</p>
+</template>
 
 <style lang="scss" scoped></style>
