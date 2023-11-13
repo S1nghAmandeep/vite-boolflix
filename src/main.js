defe6b195445900b3df5b90a9ec1162ee9e266d2
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-13 15:23:07 +0100

    imported-icons

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faBell, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faStar, farStar);
+library.add(faStar, farStar, faBell, faCaretDown);
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
